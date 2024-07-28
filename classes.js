b611311e3545368a284c4259f1c222e2cb1659d3
Reginaldo-Dev <137166839+Reginaldo-Dev@users.noreply.github.com>
2024-07-28 13:32:53 -0300

    Criando personagens

---
+++ classes.js
@@ -0,0 +1,44 @@
+// knight ou Sorcerer
+// LittleMonster ou BigMonster
+
+//Personagem principal
+class Character {
+    
+    _life = 1;
+    maxLife = 1;
+    attack = 0;
+    defense = 0;
+
+    constructor (name) {
+        this.name = name;
+    }
+
+    get life() {
+        return this._life;
+    }
+    set life(newLife) {
+        return this._life = newLife < 0 ? 0 : newLife;
+    }
+}
+
+//Personagem filho
+
+class Knight extends Character {
+    constructor(name) {
+        super(name);
+        this.life = 100;
+        this.attack = 10;
+        this.defense = 8;
+        this.maxLife = this.life
+    }
+}
+
+class Sorcerer extends Character {
+    constructor(name) {
+        super(name);
+        this.life = 80;
+        this.attack = 15;
+        this.defense = 3;
+        this.maxLife = this.life
+    }
+}
